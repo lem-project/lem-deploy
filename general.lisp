3f4b1a9d72ed08be649efbbbde3061a0075c353a
cxxxr <g23tlm@gmail.com>
2023-05-14 16:37:27 +0900

    fix

--- general.lisp
+++ general.lisp
@@ -3,6 +3,18 @@
 (setq lem::*deployed* t)
 (lem-lisp-mode/swank-modules:require-swank-modules)
 
+(dolist (module sb-impl::*modules*)
+  (require module))
+
+(setq asdf:*output-translations-parameter* nil
+      asdf/output-translations:*output-translations* nil)
+
+;; XXX:
+;; (ql:quickload :drakma) causes sb-bsd-sockets require to fail.
+;; Override this method to work around it.
+(defmethod asdf:perform ((o asdf:load-op) (s asdf:require-system))
+  nil)
+
 (defun launch (&optional (args (uiop:command-line-arguments)))
   (setup-foreign-library-directories)
   (apply #'lem:lem args))
