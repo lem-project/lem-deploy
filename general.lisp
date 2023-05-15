e320da3697f8799b3dfd1341aa3b36b2b427ce7f
cxxxr <g23tlm@gmail.com>
2023-05-15 20:18:46 +0900

    fix

--- general.lisp
+++ general.lisp
@@ -15,6 +15,28 @@
 ;; (defmethod asdf:perform ((o asdf:load-op) (s asdf:require-system))
 ;;   nil)
 
+(defmethod asdf:output-files :around ((operation t) (component t))
+  (call-next-method)
+  ;; XXX:
+  ;; Disable the following code defined in asdf
+  ;; because the target environment may refer to directories in the host environment
+  #+(or)
+  (do-asdf-cache `(output-files ,operation ,component)
+    (values
+     (multiple-value-bind (pathnames fixedp) (call-next-method)
+       ;; 1- Make sure we have absolute pathnames
+       (let* ((directory (pathname-directory-pathname
+                          (component-pathname (find-component () component))))
+              (absolute-pathnames
+                (loop
+                  :for pathname :in pathnames
+                  :collect (ensure-absolute-pathname pathname directory))))
+         ;; 2- Translate those pathnames as required
+         (if fixedp
+             absolute-pathnames
+             (mapcar *output-translation-function* absolute-pathnames))))
+     t)))
+
 (defun launch (&optional (args (uiop:command-line-arguments)))
   (setup-foreign-library-directories)
   (apply #'lem:lem args))
