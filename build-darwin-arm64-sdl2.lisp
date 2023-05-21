051aec8c33339cd71c1f417cfbb3d5b24248577b
cxxxr <g23tlm@gmail.com>
2023-05-21 23:10:33 +0900

    update

--- build-darwin-arm64-sdl2.lisp
+++ build-darwin-arm64-sdl2.lisp
@@ -5,17 +5,35 @@
 (cffi:close-foreign-library 'sdl2-ttf::libsdl2-ttf)
 (cffi:close-foreign-library 'sdl2-image::libsdl2-image)
 
+#+(or)
+(defun p (x)
+  (with-open-file (out "~/lem.log"
+                       :direction :output
+                       :if-exists :append
+                       :if-does-not-exist :create)
+    (uiop:println x out)))
+
+#+(or)
+(defun depends-libs ()
+  (loop :for lib :in (cffi:list-foreign-libraries)
+        :for lib-pathname := (cffi:foreign-library-pathname lib)
+        :unless (uiop:string-prefix-p "/System" (namestring lib-pathname))
+        :collect lib-pathname))
+
 (defun setup-foreign-library-directories ()
+  ;; (p "setup-foreign-library-directories")
+
+  ;; (setf (uiop:getenv "DYLD_FALLBACK_LIBRARY_PATH") (format nil "/usr/local/lib:/usr/lib:~A" (namestring (lem:lem-relative-pathname "../Frameworks/"))))
+  ;; (setf (uiop:getenv "DYLD_LIBRARY_PATH") (namestring (lem:lem-relative-pathname "../Frameworks/")))
+
   (uiop:chdir (user-homedir-pathname))
 
   (setf cffi:*foreign-library-directories* '())
   (setf lem-sdl2/resource::*resource-directory*
         (lem:lem-relative-pathname "../Resources/"))
 
-  (cffi:load-foreign-library (lem:lem-relative-pathname "../Frameworks/libasyncprocess.so"))
-  (cffi:load-foreign-library (lem:lem-relative-pathname "../Frameworks/libSDL2-2.0.0.dylib"))
-  (cffi:load-foreign-library (lem:lem-relative-pathname "../Frameworks/libSDL2_image-2.0.0.dylib"))
-  (cffi:load-foreign-library (lem:lem-relative-pathname "../Frameworks/libSDL2_ttf-2.0.0.dylib")))
+  (dolist (file (uiop:directory-files (lem:lem-relative-pathname "../Frameworks/")))
+    (cffi:load-foreign-library file)))
 
 (lem:copy-file-or-directory (asdf:system-relative-pathname :lem-sdl2 "resources/")
                             "darwin/arm64/lem.app/Contents/Resources/resources/")
